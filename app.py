97d73139c7bf6c6168f34f18a38699b097d1fda6
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-01-25 15:41:33 +0200

    changes in static files structure
    changes in static files structure

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ mail = Mail()
 
 # app
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
 
 app.config['SECRET_KEY'] = creds.secret_key
 
@@ -26,7 +29,7 @@ app.config['SECRET_KEY'] = creds.secret_key
 # app.config["MAIL_SERVER"] = "appssmtp.abv.bg" # abv
 # app.config["MAIL_PORT"] = 465 # 465 for SSL - abv
 
-# elastice mail
+# elastic email
 
 app.config["MAIL_SERVER"] = "smtp.elasticemail.com"
 app.config["MAIL_PORT"] = 2525
@@ -40,60 +43,67 @@ app.config["MAIL_PASSWORD"] = creds.password
 
 mail.init_app(app)
 
+
 # handle forms
+
 
 @app.route('/contacts', methods=('GET', 'POST'))
 def contacts():
-	form = ContactForm()
+    form = ContactForm()
 
-	if request.method == 'POST':
-		if form.validate() == False:
-			flash('Моля, попълнете всички полета.')
-			return render_template('contacts.html', form=form)
-		else:
-			msg = Message(form.subject.data, sender=creds.username, recipients=['georgi.asparuhov.2020@abv.bg'])
-			msg.body = """
-			От: %s \nE-mail: %s\nСъобщение: %s
-			\n
-			Сайт: Крисияна Ауто 24
-			------------------------------------------------------------------
-			""" % (form.name.data, form.email.data, form.message.data)
-			mail.send(msg)
-			
-			return render_template("message_sent.html") 
-	
-	elif request.method == 'GET':
-		return render_template('contacts.html', form=form)
+    if request.method == 'POST':
+        if not form.validate():
+            flash('Моля, попълнете всички полета.')
+            return render_template('contacts.html', form=form)
+        else:
+            msg = Message(form.subject.data, sender=creds.username, recipients=['georgi.asparuhov.2020@abv.bg'])
+            msg.body = """От: %s \nE-mail: %s\nСъобщение: %s\nСайт: Крисияна Ауто 24 
+            ------------------------------------------------------------------
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+
+            return render_template("message_sent.html")
+
+    elif request.method == 'GET':
+        return render_template('contacts.html', form=form)
+
 
 # handle 404 error
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 # handle app routes/pages
+
 
 @app.route("/")
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
+
 
 @app.route("/history")
 def history():
-	return render_template("history.html")
+    return render_template("history.html")
+
 
 @app.route("/services")
 def services():
-	return render_template("services.html")
+    return render_template("services.html")
+
 
 @app.route("/prices")
 def prices():
-	return render_template("prices.html")
+    return render_template("prices.html")
+
 
 @app.route("/terms_and_conditions")
 def terms_and_conditions():
-	return render_template("terms_and_conditions.html")
+    return render_template("terms_and_conditions.html")
 
-#
 
-if __name__=='__main__':
-	app.run(ssl_context='adhoc')
+if __name__ == '__main__':
+    app.run()
+    # app.run(ssl_context='adhoc')
--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField, SubmitField, TextField, validators, ValidationError
 
+
 class ContactForm(FlaskForm):
-  name = TextField("Име",  [validators.Required("Моля, въведете вашето име.")])
-  email = TextField("E-mail адрес",  [validators.Required("Моля, въведете вашия имейл адрес."), validators.Email("Моля, въведете вашия имейл адрес.")])
-  subject = TextField("Тема",  [validators.Required("Моля, въведете тема.")])
-  message = TextAreaField("Съобщение",  [validators.Required("Моля, въведете съобщение.")])
-  submit = SubmitField("Изпрати")
+    name = TextField("Име", [validators.DataRequired("Моля, въведете вашето име.")])
+    email = TextField("E-mail адрес", [validators.DataRequired("Моля, въведете вашия имейл адрес."),
+                                       validators.Email("Моля, въведете вашия имейл адрес.")])
+    subject = TextField("Тема", [validators.DataRequired("Моля, въведете тема.")])
+    message = TextAreaField("Съобщение", [validators.DataRequired("Моля, въведете съобщение.")])
+    submit = SubmitField("Изпрати")
