ec04849f9eb879f2bd3ad51572ad6966d81f256a
kristiyanivanovx <kristianiva88@gmail.com>
2021-06-16 11:59:49 +0300

    Update creds.py

--- creds.py
+++ creds.py
@@ -4,8 +4,8 @@ secret_key = os.urandom(16)
 
 # elastic email
 
-username = 'just.a.test.123.321.app@gmail.com'
-password = '508347E97BF670238A82F82B4AE3B92E2DBC'
+# username = 'just.a.test.123.321.app@gmail.com'
+# password = '508347E97BF670238A82F82B4AE3B92E2DBC'
 
 # others
 
@@ -15,5 +15,5 @@ password = '508347E97BF670238A82F82B4AE3B92E2DBC'
 # username = 'georgi.asparuhov.2020@abv.bg' # abv
 # password = 'gecaASPARUHOV123!!!' # abv
 
-# username = 'just.a.test.123.321.app@gmail.com' # google
-# password = 'testsite!123' # google
+ username = 'just.a.test.123.321.app@gmail.com' # google
+ password = 'testsite!123' # google
